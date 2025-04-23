86ba0154a5454fc4c43b2ecadd37875e41f29f63
28071233 <28071233@students.lincoln.ac.uk>
2025-04-23 17:02:01 +0100

    Completed basic setup of Creature, Monster and PlayerRewritten classes

--- Creature.cs
+++ Creature.cs
@@ -12,14 +12,54 @@ namespace DungeonExplorer
         public string Description;
         public int Health;
 
-        public Creature()
+        public Creature(string name, string description, int health)
         {
-            this.Name = "null";
-            this.Description = "null";
-            this.Health = 0;
+            this.Name = name;
+            this.Description = description;
+            this.Health = health;
         }
     }
 
+    public class Monster : Creature
+    {
+        public int AttackBaseDamage;
+        public bool Hasloot;
 
+        public Monster(string Name, string Description, int Health, int attackBaseDamage, bool hasloot)
+            : base(Name, Description, Health)
+        {
+            this.Name = Name;
+            this.Description = Description;
+            this.Health = Health;
+            this.AttackBaseDamage = attackBaseDamage;
+            this.Hasloot = hasloot;
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine("The Monster attacked!");
+        }
+
+        public void SpecialAttack()
+        {
+            Console.WriteLine("The Monter used it special ability!");
+        }
+
+        public void Flee()
+        {
+            Console.WriteLine("The Monster Fleed!");
+        }
+    }
+
+    public class PlayerRewritten : Creature
+    {
+        public PlayerRewritten(string name, string description, int health) 
+            : base(name, description, health)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Health = health;
+        }
+    }
 }
 
